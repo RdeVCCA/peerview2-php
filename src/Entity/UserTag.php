33b1018cb047f5af4643d984a2970febd208150f
h-isthebestletter <102966545+h-isthebestletter@users.noreply.github.com>
2025-12-14 10:32:03 +0800

    add the tag field

--- src/Entity/UserTag.php
+++ src/Entity/UserTag.php
@@ -17,6 +17,9 @@ class UserTag
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $tag = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +33,18 @@ class UserTag
     public function setUser(?User $user): static
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTag(): ?string
+    {
+        return $this->tag;
+    }
+
+    public function setTag(string $tag): static
+    {
+        $this->tag = $tag;
 
         return $this;
     }
